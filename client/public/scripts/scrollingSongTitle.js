1f139aecd543d8e5aa4bfa9355291f05d52370a0
Dave May <echo.dave@protonmail.com>
2021-07-11 23:56:31 -0400

    comment clenaup

--- client/public/scripts/scrollingSongTitle.js
+++ client/public/scripts/scrollingSongTitle.js
@@ -1,6 +1,3 @@
-// let getScrollLength = (el) =>{ return document.querySelector(el).scrollWidth};
-// let getClientlLength = (el) =>{ return document.querySelector(el).clientWidth};
-
 const scrollingSongTitle = (el) => {
     let getScrollLength = (el) =>{ return el.scrollWidth};
     let getClientlLength = (el) =>{ return el.clientWidth};
