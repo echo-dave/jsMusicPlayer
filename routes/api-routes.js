ff2bba94ced6e9b4b949bdae33e34c9e1a91f769
Dave May <echo.dave@protonmail.com>
2021-07-14 17:45:58 -0400

    correct music route to get and update to use .env for dir path

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,12 @@
 const readMusicDir = require("../utils/readMusicDir.js");
 
 module.exports = function(app) {
-    app.post("/api/music", async (req,res) => {
-        console.log(`req: \n`, req.body);
-        if (req.body) {
-        let music = await readMusicDir(req.body.dir);
+    app.get("/api/music", async (req,res) => {
+        console.log(`req: \n`, process.env.MUSICDIR);
+        let music = await readMusicDir(process.env.MUSICDIR);
         console.log(`server res:`, music);
         res.json(music);
-        }
+        
     })
     
 }
