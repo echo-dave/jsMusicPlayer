43cd21dbc6cfa42d2e62792132e5a1d4b78b9c98
Dave May <echo.dave@protonmail.com>
2021-07-14 17:44:56 -0400

    move the music dir to .env and correct fetch to get request

--- client/public/scripts/tracklist.js
+++ client/public/scripts/tracklist.js
@@ -1,12 +1,12 @@
-const dir = {dir:'client/public/music/'};
-const request = new Request('/api/music',{method: 'post',body: JSON.stringify(dir), headers: {'Content-type': 'application/json'}});
+// const dir = {dir:'client/public/music/'};
+// const request = new Request('/api/music',{method: 'post',body: JSON.stringify(dir), headers: {'Content-type': 'application/json'}});
 
 let trackData; 
 
 let trackList = document.querySelector("#trackList");
 
 const buildTracklist = () => { 
-    fetch(request).then(res => {
+    fetch('/api/music').then(res => {
         // console.log('status call', res.status, '\n', 'res.result:', res.result, '\n ----\n', 'res',  res.json());
         if (res.status === 200) {
             res.json().then(res => {
