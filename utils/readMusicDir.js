d3be0bc534a359e7f0dc39f28b72920469924d82
Dave May <echo.dave@protonmail.com>
2021-07-14 23:12:43 -0400

    remove comments use environment variable for music dir url

--- utils/readMusicDir.js
+++ utils/readMusicDir.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 
 let files;
 let musicFiles = [];
-const playbackDir = '/music/';
+
 module.exports = async (dir)  => {
     if (musicFiles.length === 0) {
     try { 
@@ -13,11 +13,8 @@ module.exports = async (dir)  => {
             console.log(`--- \n`, file, `\n ---`);
 
             if (file.isFile()) {
-                console.log(`${file.name} is a file to process`)
-                musicFiles.push({url: playbackDir+file.name,title: file.name.slice(0,-4), id: index})
-            } else {
-                console.log(`${file.name} is not a file`);
-            }             
+                musicFiles.push({url: process.env.MUSICDIR_PUBLIC_URL+'/'+file.name,title: file.name.slice(0,-4), id: index})
+            }            
         })
     } catch (err) {
     console.error(err);
