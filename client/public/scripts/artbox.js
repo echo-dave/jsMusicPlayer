ca17834c1c0a0c723fdc5b6ff393483b842de8c9
Dave May <echo.dave@protonmail.com>
2021-07-30 02:44:08 -0400

    added modal artbox for tiny screens work #7

--- client/public/scripts/artbox.js
+++ client/public/scripts/artbox.js
@@ -22,6 +22,25 @@ function toggleArtBox(){
             insertPosition  = 'afterbegin';
             appendArtBoxToPlayer();
         }
+        //use a modal
+        if (player.clientHeight + player.clientWidth >= window.innerHeight) {
+            console.log('make modal');
+            document.querySelector('body').insertAdjacentHTML('beforeend',`
+            <div id="artBox" style="height:${window.innerHeight * .9 + 'px'};
+            width:${window.innerHeight * .9 + 'px'};
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            background:grey;
+            z-index:2;
+            box-shadow: 0 0 1.5em .5em rgba(0,0,0,.5);
+            border-radius: .5em;
+            transform: translate(-50%, -50%);">
+            &nbsp
+            </div>
+            `)
+            closeArtBox();
+        }
 
 
     function appendArtBoxToPlayer() {
