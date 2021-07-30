9cba73cc21356dd2069c356ea66d650cc2766fa4
Dave May <echo.dave@protonmail.com>
2021-07-30 01:54:22 -0400

    work #7 conditionally positioning artbox for mobile and desktop

--- client/public/scripts/artbox.js
+++ client/public/scripts/artbox.js
@@ -3,19 +3,44 @@ songTitle.onclick = toggleArtBox;
 
 function toggleArtBox(){
     const player = document.querySelector('#player');
+    player.styles = getComputedStyle(player);
+    console.log('bottom: ' + player.style.bottom);
 
     if (!document.querySelector("#artBox")){
-    player.insertAdjacentHTML("afterbegin",
-    `
-    <div id="artBox" style="width:100%;background:grey">&nbsp;</div>
-    `)
-    const artBox = document.querySelector("#artBox")
-    artBox.style.height = artBox.clientWidth + 'px';
-    console.log(`width:  ${artBox.clientWidth}`);
+        console.log('artBox!');
+        const body = document.querySelector('body')
+        let insertPosition = "";
+        //append arbox to bottom of player
+        if (player.clientHeight + player.clientWidth < window.innerHeight && player.styles.top !== 'auto') {
+            console.log('beforeend');
+            insertPosition = 'beforeend';
+            appendArtBoxToPlayer();
+        }
+        //append arbox to top of player
+        if (player.clientHeight + player.clientWidth < window.innerHeight && player.styles.bottom !== 'auto') {
+            console.log('afterbegin');
+            insertPosition  = 'afterbegin';
+            appendArtBoxToPlayer();
+        }
 
+
+    function appendArtBoxToPlayer() {
+        player.insertAdjacentHTML(insertPosition,
+        `
+        <div id="artBox" style="width:100%;background:grey">&nbsp;</div>
+        `)
+        const artBox = document.querySelector("#artBox")
+        artBox.style.height = artBox.clientWidth + 'px';
+        console.log(`width:  ${artBox.clientWidth}`);
+        closeArtBox();
+
+    }
+
+    } else document.querySelector("#artBox").remove();
+}
+
+function closeArtBox () {
     artBox.addEventListener('click', () =>{
         artBox.remove();
     }, {once: true})
-
-    } else document.querySelector("#artBox").remove();
 }
