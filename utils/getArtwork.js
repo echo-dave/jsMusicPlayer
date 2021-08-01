2e5fba7f84ede871af6509c249ea9f1e86a0af89
Dave May <echo.dave@protonmail.com>
2021-08-01 04:29:29 -0400

    work #7 server logic for getting jpgs from music folder and catching errors

---
+++ utils/getArtwork.js
@@ -0,0 +1,19 @@
+const fs = require('fs/promises');
+const path = require('path');
+module.exports = async (trackName) => {
+    const trackPath = path.resolve(process.env.MUSICDIR +'/'+ trackName);
+    try {
+    exists = await fs.stat(trackPath);
+    if (exists.isFile()) return trackPath
+    } catch (err) {
+        function NoArtwork (message, status) {
+            this.message = message;
+            this.status = 204;
+            this.name = "ArtError";
+        }
+        if (err.code == 'ENOENT') {
+            throw new NoArtwork ("no artwork");
+        }
+        else return err
+    }
+}
