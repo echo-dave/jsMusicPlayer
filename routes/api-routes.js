ce0b04688452cf79a06a55eed78adf4eb0f05e47
Dave May <echo.dave@protonmail.com>
2021-08-01 04:26:34 -0400

    work #7 sending jpgs not path string, work on error msgs

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const readMusicDir = require("../utils/readMusicDir.js");
-// const getArtwork = require("../utils/getArtwork.js");
+const getArtwork = require("../utils/getArtwork.js");
+
 
 module.exports = function(app) {
     app.get("/api/music", async (req,res) => {
@@ -15,17 +17,21 @@ module.exports = function(app) {
         }
         
     })
-    
+
     app.get("/api/artwork/:name", async (req,res) => {
         try {
         console.log(`art request name: ${req.params.name}`);
-        let artwork = await getArtwork(process.env.MUSICDIR);
+        let artwork = await getArtwork(req.params.name + '.jpg');
         console.log('artwork res: ', artwork);
-        res.json(artwork)
+        res.set({'content-type': 'image/jpeg'}).status('200');
+        fs.createReadStream(artwork).pipe(res);
+
+        // res.sendFile(artwork, {dotfiles: "ignore"});
         }
         catch (error) {
             console.log('Server error: \n', error);
-            res.status(404).json({message: error.message})
+            if (error.status === 204) res.status(204).send(error.message)
+            else res.status(500).send(error)
         }
 
     })
