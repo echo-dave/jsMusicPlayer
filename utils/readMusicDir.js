6554055ae23eb44f5db7b25ca5a02fb7801d3289
Dave May <echo.dave@protonmail.com>
2021-08-01 04:28:08 -0400

    work #7 making sure we only process mp3 files

--- utils/readMusicDir.js
+++ utils/readMusicDir.js
@@ -7,12 +7,14 @@ module.exports = async (dir)  => {
     if (musicFiles.length === 0) {
     try { 
         files = await fs.readdir(dir,{withFileTypes:true});
-        console.log(`files: ${files} \n`);
+        console.log(`files: `, files);
 
         files.forEach((file,index) => {
-            console.log(`--- \n`, file, `\n ---`);
-
-            if (file.isFile()) {
+            console.log(`\n`, file, `\n ---`);
+            let ext = file.name.length - file.name.lastIndexOf('.');
+            ext = file.name.slice(-4);
+            console.log('ext ', ext);
+            if (file.isFile() && ext === '.mp3') {
                 musicFiles.push({url: process.env.MUSICDIR_PUBLIC_URL+'/'+file.name,title: file.name.slice(0,-4), id: index})
             }            
         })
@@ -20,7 +22,7 @@ module.exports = async (dir)  => {
     console.error(err);
     }
     }
-    console.log(`music ${musicFiles}\n`);
+    console.log(`music: `, musicFiles);
     return musicFiles;
 
 
