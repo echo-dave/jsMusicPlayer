069076032c95ae6c8cb6d6015c72839b96678ec7
Dave May <echo.dave@protonmail.com>
2021-10-31 17:17:19 -0400

    fix 18 try catch flow for default image fallback

--- utils/getArtwork.js
+++ utils/getArtwork.js
@@ -4,23 +4,22 @@ module.exports = async (trackName) => {
     const defaultImage = path.resolve(process.env.MUSICDIR +'/defaultImage.jpg')
     const trackPath = path.resolve(process.env.MUSICDIR +'/'+ trackName);
     try {
-    const exists = await fs.stat(trackPath);
-    if (exists.isFile()) {return trackPath}
-    else {
-        const defaultExists = await fs.stat(defaultImage)
-        if (defaultExists.isFile()) {
-            return defaultImage
-        }
-    }
+        await fs.stat(trackPath);
+            return trackPath
     } catch (err) {
-        function NoArtwork (message) {
-            this.message = message;
-            this.status = 204;
-            this.name = "ArtError";
+        try {
+            await fs.stat(defaultImage)
+            return defaultImage
+        } catch (err) {
+            function NoArtwork (message) {
+                this.message = message;
+                this.status = 204;
+                this.name = "ArtError";
+            }
+            if (err.code == 'ENOENT') {
+                throw new NoArtwork ("no artwork");
+            }
+            else return err
         }
-        if (err.code == 'ENOENT') {
-            throw new NoArtwork ("no artwork");
-        }
-        else return err
     }
 }
